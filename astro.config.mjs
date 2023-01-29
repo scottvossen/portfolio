06bfa3f6769d212dbe0f43683caef5ddc45505af
VOSSEN-LEGION\scott <scottvossen@gmail.com>
2023-01-28 21:28:08 -0600

    prefix all urls with portfolio

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,5 +8,6 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://scottvossen.github.io',
+  base: '/portfolio',
   integrations: [mdx(), sitemap(), tailwind(), react()]
 });
