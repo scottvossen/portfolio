900b562234fdaf3de8719107d965e3a5deecd35c
VOSSEN-LEGION\scott <scottvossen@gmail.com>
2023-01-28 19:07:16 -0600

    Add prettier and fix react rendering

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,14 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	daisyui: {
-		themes: true,
-	},
-	plugins: [
-		require("@tailwindcss/typography"),
-		require("daisyui")
-	],
-}
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {}
+  },
+  daisyui: {
+    themes: true
+  },
+  plugins: [require('@tailwindcss/typography'), require('daisyui')]
+};
