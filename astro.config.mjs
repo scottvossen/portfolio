b334165ee2e923bc69827d6c5d414a259eb8c5e0
VOSSEN-LEGION\scott <scottvossen@gmail.com>
2023-01-28 22:41:18 -0600

    put base config back

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://scottvossen.github.io/portfolio',
-  // base: '/portfolio',
+  site: 'https://scottvossen.github.io',
+  base: '/portfolio',
   integrations: [mdx(), sitemap(), tailwind(), react()]
 });
